532763f804008174c67a8bb67bff41db4b3ff587
Tung Nguyen <tongueroo@gmail.com>
2018-01-04 15:45:35 -0800

    account for elbs using security groups

--- lib/aws_inventory/security_group/shared.rb
+++ lib/aws_inventory/security_group/shared.rb
@@ -1,10 +1,19 @@
 module AwsInventory::SecurityGroup::Shared
   def used_security_groups
     groups = instances.inject([]) do |results, i|
-      results += i.security_groups
+      results += i.security_groups # returns Aws::EC2::Types::GroupIdentifier
       results
     end
-    groups.uniq(&:group_id)
+    instance_groups = groups.uniq(&:group_id)
+
+    v1_group_ids = elbv1.describe_load_balancers.load_balancer_descriptions.map(&:security_groups).flatten
+    v2_group_ids = elbv2.describe_load_balancers.load_balancers.map(&:security_groups).flatten
+    group_ids = v1_group_ids + v2_group_ids
+    elb_groups = ec2.describe_security_groups(group_ids: group_ids).security_groups  # returns Aws::EC2::Types::SecurityGroup
+
+    # Ducktyping: the types are different by they both respond to the group_id method.
+    all_groups = instance_groups + elb_groups
+    all_groups.uniq(&:group_id)
   end
 
   def unused_security_groups
