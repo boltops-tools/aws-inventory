d6138ab42efd6ef9026c65b7e2ddb5a9b0722533
Tung Nguyen <tongueroo@gmail.com>
2018-01-04 16:24:57 -0800

    fix unused security groups for elbs

--- lib/aws_inventory/security_group/shared.rb
+++ lib/aws_inventory/security_group/shared.rb
@@ -8,8 +8,8 @@ module AwsInventory::SecurityGroup::Shared
 
     v1_group_ids = elbv1.describe_load_balancers.load_balancer_descriptions.map(&:security_groups).flatten
     v2_group_ids = elbv2.describe_load_balancers.load_balancers.map(&:security_groups).flatten
-    group_ids = v1_group_ids + v2_group_ids
-    elb_groups = ec2.describe_security_groups(group_ids: group_ids).security_groups  # returns Aws::EC2::Types::SecurityGroup
+    elb_group_ids = v1_group_ids + v2_group_ids
+    elb_groups = security_groups.select { |sg| elb_group_ids.include?(sg.group_id) } # returns Aws::EC2::Types::SecurityGroup
 
     # Ducktyping: the types are different by they both respond to the group_id method.
     all_groups = instance_groups + elb_groups
